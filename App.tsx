dea3c771c0a40474b5bd222c6c1fa012cf52bc9c
James Collins <james.collins@mattr.global>
2023-01-19 15:14:10 +1300

    feat: style typing

--- App.tsx
+++ App.tsx
@@ -1,44 +1,47 @@
-import MaskedView from "@react-native-masked-view/masked-view";
-import { LinearGradient } from "react-native-linear-gradient";
-import { range } from "lodash";
-import React, { useState } from "react";
-import { Image, ImageBackground, ScrollView, StyleSheet, Text, View, ViewProps } from "react-native";
-import { GradientBorderView } from "./package/index";
+import MaskedView from '@react-native-masked-view/masked-view';
+import {LinearGradient} from 'react-native-linear-gradient';
+import {range} from 'lodash';
+import React from 'react';
+import {
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  ViewProps,
+} from 'react-native';
+import {GradientBorderView} from './package/index';
 
 function Giraffe() {
   return (
-
     <MaskedView
-      style={{ flex: 1 }}
-      maskElement={(
-        <View
-          style={[StyleSheet.absoluteFill]}
-        >
+      style={{flex: 1}}
+      maskElement={
+        <View style={[StyleSheet.absoluteFill]}>
           <ImageBackground
             style={[StyleSheet.absoluteFill]}
-            resizeMode='contain'
+            resizeMode="contain"
             source={require('./giraffe.png')}
           />
         </View>
-
-      )}
-    >
+      }>
       <LinearGradient
         colors={['red', 'blue']}
         style={StyleSheet.absoluteFill}
-        pointerEvents='none'
+        pointerEvents="none"
       />
     </MaskedView>
-
-  )
+  );
 }
-export function FadeOutTopView({ fadeHeight = 100, ...props }: ViewProps & { fadeHeight?: number }) {
+export function FadeOutTopView({
+  fadeHeight = 100,
+  ...props
+}: ViewProps & {fadeHeight?: number}) {
   return (
-    <MaskedView style={{ flex: 1, }}
-      maskElement={(
-        <View
-          style={StyleSheet.absoluteFill}
-        >
+    <MaskedView
+      style={{flex: 1}}
+      maskElement={
+        <View style={StyleSheet.absoluteFill}>
           <LinearGradient
             colors={['rgba(0, 0, 0, 0)', 'rgba(0,0,0,1)']}
             style={{
@@ -50,32 +53,36 @@ export function FadeOutTopView({ fadeHeight = 100, ...props }: ViewProps & { fad
             }}
           />
           <View
-            style={{ position: 'absolute', top: fadeHeight, left: 0, right: 0, bottom: 0, backgroundColor: 'black', }}
+            style={{
+              position: 'absolute',
+              top: fadeHeight,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: 'black',
+            }}
           />
         </View>
-      )}
+      }
       {...props}
     />
-  )
+  );
 }
 
 function FadeOut() {
   return (
     <View>
-
-      <Text style={{
-        color: 'white',
-        fontSize: 40,
-        marginLeft: 24,
-        marginTop: 50,
-      }}>
+      <Text
+        style={{
+          color: 'white',
+          fontSize: 40,
+          marginLeft: 24,
+          marginTop: 50,
+        }}>
         A Fade Effect
       </Text>
-      <FadeOutTopView style={{ flex: 1 }}>
-        <ScrollView
-          contentContainerStyle={{ padding: 16, }}
-        >
-
+      <FadeOutTopView style={{flex: 1}}>
+        <ScrollView contentContainerStyle={{padding: 16}}>
           {range(10).map(e => (
             <GradientBorderView
               style={{
@@ -86,30 +93,32 @@ function FadeOut() {
                 marginBottom: 20,
               }}
               gradientProps={{
-                colors: ['red', 'orange']
-              }}
-            >
-              <Text style={{
-                fontSize: 24,
-                marginBottom: 16
+                colors: ['red', 'orange'],
               }}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  marginBottom: 16,
+                }}>
                 Title
               </Text>
-              <Text style={{ fontSize: 15 }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              <Text style={{fontSize: 15}}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
               </Text>
             </GradientBorderView>
           ))}
-
         </ScrollView>
       </FadeOutTopView>
-
     </View>
-  )
+  );
 }
 
 const App = () => {
-
   return (
     <View
       style={{
@@ -117,8 +126,7 @@ const App = () => {
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
-      }}
-    >
+      }}>
       {/* <View
         style={StyleSheet.absoluteFill}
       >
@@ -174,7 +182,7 @@ const App = () => {
 
       <GradientBorderView
         gradientProps={{
-          colors: ['red', 'orange']
+          colors: ['red', 'orange'],
         }}
         style={{
           borderWidth: 5,
@@ -193,9 +201,7 @@ const App = () => {
         }}
       /> */}
     </View>
-  )
+  );
 };
-
-
 
 export default App;
